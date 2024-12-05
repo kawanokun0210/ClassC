f1b40541c587b60f0f35d97fc42d9361bcf052b6
kawanokun0210 <115131957+kawanokun0210@users.noreply.github.com>
2024-12-05 15:15:03 +0900

    終わり

---
+++ Item.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemTest
+{
+    internal class Item
+    {
+        //フィールド
+        private string name;
+
+        //メソッド
+        //コンストラクタ
+        public Item (string name)
+        {
+            this.name = name;
+        }
+
+        //何かを使う
+        public void Use()
+        {
+            Console.WriteLine("{0}を使った", name);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ using FlyingRobotTest;
 using TankRobotTest;
 using System.Xml.Linq;
 using RobotTest;
+using ItemTest;
+using System.ComponentModel;
 
 namespace ClassC
 {
@@ -16,36 +18,9 @@ namespace ClassC
     {
         static void Main(string[] args)
         {
-            //乱数発生器の生成
-            Random random = new Random(Environment.TickCount);
-            //Listコレクションnumbersを作成する
-            List<Robot> robots = new List<Robot>();
-            //ランダムの数字を格納
-            int randomNum = 0;
+            Item item = new Item("つるはし");
 
-            //サイコロを15回振る
-            for(int i = 0; i < 15; i++)
-            {
-                //ランダムの数字を抽選する
-                randomNum = random.Next(0, 1 + 1);
-
-                //数字が0ならタンクロボを出す
-                if(randomNum == 0)
-                {
-                    robots.Add(new TankRobot("タンクロボ"));
-                }
-                else//0じゃなければ空飛ぶロボを出す
-                {
-                    robots.Add(new FlyingRobot("空飛ぶロボ"));
-                }
-
-            }
-
-            //全ての中身を確認
-            foreach(Robot robot in robots)
-            {
-                robot.Attack();
-            }
+            item.Use();
 
             //一時停止
             Console.ReadLine();
